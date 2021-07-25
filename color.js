17fffff677b862c6a6d74e6a61bd72e3e2dd13de
Bharath S R <64520959+bharathrajb6@users.noreply.github.com>
2021-07-25 10:12:02 +0530

    Add files via upload

---
+++ color.js
@@ -0,0 +1,18 @@
+
+let x=document.getElementsByTagName("BODY")[0];
+function red()
+{
+	x.style.backgroundColor="#E74C3C";
+}
+function green()
+{
+	x.style.backgroundColor="#27AE60";
+}
+function blue()
+{
+	x.style.backgroundColor="#3498DB";
+}
+function purple()
+{
+	x.style.backgroundColor="#AF7AC5";
+}
